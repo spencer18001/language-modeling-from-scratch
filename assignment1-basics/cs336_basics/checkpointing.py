f8643373e2862699d8bb1e214f45bd27b8837dca
spencer18001 <spencer18001@gmail.com>
2026-07-13 21:49:06 +0800

    checkpoint

---
+++ assignment1-basics/cs336_basics/checkpointing.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+import os
+from typing import IO, BinaryIO
+
+import torch
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | BinaryIO | IO[bytes],
+) -> None:
+    checkpoint = {
+        "model": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+        "iteration": iteration,
+    }
+    torch.save(checkpoint, out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | BinaryIO | IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint["model"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    return checkpoint["iteration"]
